fcae46bf8ea98d88009e20a16c3d01d09accf3fc
Maciej Łącki <maciej.lacky@gmail.com>
2021-03-07 18:39:49 +0100

    Add seeds new data to databse using migrations

---
+++ RestaurantAPI/RestaurantSeeder.cs
@@ -0,0 +1,68 @@
+﻿using RestaurantAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI
+{
+    public class RestaurantSeeder
+    {
+        private readonly RestaurantDbContext _dbContext;
+        public RestaurantSeeder(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                if (!_dbContext.Restaurants.Any())
+                {
+                    var restaurants = GetRestaurants();
+                    _dbContext.Restaurants.AddRange(restaurants);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+
+        private IEnumerable<Restaurant> GetRestaurants()
+        {
+            var restaurants = new List<Restaurant>()
+            {
+                new Restaurant()
+                {
+                    Name = "KFC",
+                    Category = "FastFood",
+                    Description = "American fast food",
+                    ContactEmail = "main@kfc.pl",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Chicken wings",
+                            Price = 10.30M,
+                        },
+                        new Dish()
+                        {
+                            Name = "CocaCola",
+                            Price = 5.30M,
+                        },
+                    },
+                    Address = new Address()
+                    {
+                        City = "Kraków",
+                        Street = "Długa 5",
+                        PostalCode = "30-230",
+                    }
+
+
+                }
+            };
+
+            return restaurants;
+        }
+
+    }
+}
