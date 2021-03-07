8d2740b2ea38b22d63d547285f975eb1ac177731
Maciej Łącki <maciej.lacky@gmail.com>
2021-03-07 17:55:51 +0100

    Add project files.

---
+++ RestaurantAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get( int count, int minTemperature, int maxTemperature)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, count).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(minTemperature, maxTemperature),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+       [HttpPost("generate")]
+       public ActionResult<IEnumerable<WeatherForecast>> Generate([FromQuery]int count,[FromBody] TemperatureRequest request)
+        {
+            if (count < 0 || request.Max < request.Min)
+            {
+                return BadRequest();
+            }
+
+            var result = Get(count, request.Min, request.Max);
+
+            return Ok(result);
+        }
+
+    }
+}
