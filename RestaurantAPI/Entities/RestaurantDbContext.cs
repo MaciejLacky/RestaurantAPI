1313525408955629acc005d9e9f01eeb4b877555
Maciej Łącki <maciej.lacky@gmail.com>
2021-03-07 18:13:05 +0100

    Add migrations

--- RestaurantAPI/Entities/RestaurantDbContext.cs
+++ RestaurantAPI/Entities/RestaurantDbContext.cs
@@ -8,6 +8,7 @@ namespace RestaurantAPI.Entities
 {
     public class RestaurantDbContext : DbContext
     {
+        private string _connectionString = "Server = DESKTOP-4T3IM66\\SQLEXPRESS01;Database=RestaurantDb;Trusted_Connection=True;";
         public DbSet<Restaurant> Restaurants { get; set; }
         public DbSet<Dish> Dishes { get; set; }
         public DbSet<Address> Addresses { get; set; }
@@ -15,7 +16,7 @@ namespace RestaurantAPI.Entities
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_connectionString);
         }
 
     }
