caa8407e90216a518b24204822bfc9ab3faac258
Maciej Łącki <maciej.lacky@gmail.com>
2021-03-07 18:46:56 +0100

    Add require fields to address in database

--- RestaurantAPI/Entities/RestaurantDbContext.cs
+++ RestaurantAPI/Entities/RestaurantDbContext.cs
@@ -13,6 +13,17 @@ namespace RestaurantAPI.Entities
         public DbSet<Dish> Dishes { get; set; }
         public DbSet<Address> Addresses { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>()
+                  .Property(a => a.City)
+                  .IsRequired()
+                  .HasMaxLength(50);
+            modelBuilder.Entity<Address>()
+                 .Property(a => a.Street)
+                 .IsRequired()
+                 .HasMaxLength(50);
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
