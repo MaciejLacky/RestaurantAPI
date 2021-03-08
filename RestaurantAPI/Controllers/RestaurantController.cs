022fa11874b90ce838cf6c142f0ef6517348f05b
Maciej Łącki <maciej.lacky@gmail.com>
2021-03-08 18:52:21 +0100

    add restaurant controller and automapper to map class for client

---
+++ RestaurantAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/restaurant")]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly RestaurantDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public RestaurantController(RestaurantDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+        [HttpPost]
+        public ActionResult CreateRestaurant([FromBody] CreateRestaurantDto dto)
+        {
+            var restaurant = _mapper.Map<Restaurant>(dto);
+            _dbContext.Restaurants.Add(restaurant);
+            _dbContext.SaveChanges();
+            return Created($"/api/restaurant/{restaurant.Id}", null);
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<RestaurantDto>> GetAll()
+        {
+            var restaurants =_dbContext
+                .Restaurants
+                .Include(x =>x.Address)
+                .Include(x =>x.Dishes)
+                .ToList();
+
+            var restaurantsDtos = _mapper.Map<List<RestaurantDto>>(restaurants);
+        
+            
+
+            return Ok(restaurantsDtos);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<RestaurantDto>> Get([FromRoute]int id)
+        {
+            var restaurants = _dbContext
+                .Restaurants.FirstOrDefault(x => x.Id == id);
+
+            if (restaurants is null)
+            {
+                return NotFound();
+            }
+            var restaurantDto = _mapper.Map<List<RestaurantDto>>(restaurants);
+            return Ok(restaurantDto);
+        }
+    }
+}
